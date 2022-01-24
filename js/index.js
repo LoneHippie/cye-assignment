e29bdf3e0d0863c48c8a4fb868aa571db668de86
LoneHippie <lonehippie.music@gmail.com>
2022-01-24 18:05:57 +0200

    added responsive nav

--- js/index.js
+++ js/index.js
@@ -5,8 +5,28 @@ const DOM = {
     statNum2: document.getElementById('stat-num-2'),
     statCard1: document.getElementById('stat-card-1'),
     statCard2: document.getElementById('stat-card-2'),
-    statCard3: document.getElementById('stat-card-3')
+    statCard3: document.getElementById('stat-card-3'),
+
+    navToggle: document.getElementById('nav-toggle'),
+    navDesktop: document.getElementById('nav-desktop'),
+    navMobile: document.getElementById('nav-mobile')
 };
+
+let windowDimensions = window.innerWidth;
+let isScreenMobile = setVisibleNav();
+
+function setVisibleNav() {
+    if (windowDimensions > 568) { //desktop mode
+        DOM.navDesktop.classList.add('active');
+        DOM.navMobile.classList.remove('active');
+        return false;
+    } else { //mobile mode
+        DOM.navDesktop.classList.remove('active');
+        DOM.navMobile.classList.remove('active');
+        return true;
+    }
+}
+setVisibleNav();
 
 const onScreenObserver = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
@@ -27,3 +47,9 @@ onScreenObserver.observe(DOM.statCard3);
 
 //on load set scroll position
 DOM.slider.scrollLeft = 150;
+
+DOM.navToggle.addEventListener('click', (e) => {
+    if (isScreenMobile) {
+        DOM.navMobile.classList.toggle('active')
+    }
+})
